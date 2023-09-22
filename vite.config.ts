93ace867dc3c8a0b96c0d57cd3f51fdff12efb1b
freedom <459566948@qq.com>
2023-09-22 17:39:06 +0800

    配置文件修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,40 @@
-import { defineConfig } from 'vite';
+import { defineConfig, ConfigEnv, UserConfig, loadEnv, PluginOption } from 'vite';
 import react from '@vitejs/plugin-react';
 import requireTransform from 'vite-plugin-require-transform';
 import { resolve } from 'path';
+import { wrapperEnv } from './src/utils/getEnv.ts';
+import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+export default defineConfig((mode: ConfigEnv): UserConfig =>{
+  const env = loadEnv(mode.mode, process.cwd());
+  const viteEnv = wrapperEnv(env);
+  const define = {
+    "process.env": process.env
+  };
+  if (mode.mode === "development") {
+    dotenv.config({ path: ".env.development"});
+  } else if (mode.mode === "production") {
+    dotenv.config({ path: ".env.production"})
+  }
+  return {
+    define: define,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
     },
-  },
-  plugins: [
-    react(),
-    requireTransform({
-      fileRegex: /.ts$|.tsx$|.vue$/,
-    }),
-  ],
+    server: {
+      host: "0.0.0.0",
+      port: viteEnv.VITE_PORT,
+      open: viteEnv.VITE_OPEN,
+      cors: true,
+    },
+    plugins: [
+      react(),
+      requireTransform({
+        fileRegex: /.ts$|.tsx$|.vue$/,
+      }),
+    ],
+  }
 });
