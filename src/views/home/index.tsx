2a51b5f7257a6a548be96fcd620adf5d6bea9116
freedom <459566948@qq.com>
2023-09-22 16:34:40 +0800

    相关配置文件修改

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,9 +1,11 @@
 const HomeView = () => {
-  return (
-    <div>
-      <div>Test</div>
-    </div>
-  );
+	const test = () => {};
+	return (
+		<div>
+			<div>Test</div>
+			<div>111</div>
+		</div>
+	);
 };
 
 export default HomeView;
