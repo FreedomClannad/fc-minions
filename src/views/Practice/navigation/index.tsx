0d2a6455d485f5dc0f64c3c385ee9b22afb729c2
freedom <459566948@qq.com>
2023-11-04 12:58:48 +0800

    带有动画效果选中流程的动画效果流程

--- src/views/Practice/navigation/index.tsx
+++ src/views/Practice/navigation/index.tsx
@@ -1,36 +1,80 @@
 import "./index.scss";
-const floatingMenu = () => {
-	return (
-		<div className="navigation">
-			<ul>
-				<li>
-					<a href="#">
-						<span className="iconfont "></span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<span className="iconfont "></span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<span className="iconfont "></span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<span className="iconfont "></span>
-					</a>
-				</li>
-				<li>
-					<a href="#">
-						<span className="iconfont "></span>
-					</a>
-				</li>
-			</ul>
-		</div>
-	);
+import { Icon } from "@iconify-icon/react";
+import { useEffect, useRef } from "react";
+const navigateView = () => {
+	// 以下方式是为了练习其他方式绑定以及相关的js方式，正经不会这样写，
+	const navigateRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		if (navigateRef.current) {
+			const list = navigateRef.current.querySelectorAll("li");
+			const activeLink = (li: HTMLLIElement) => {
+				list.forEach(item => {
+					console.log(item);
+
+					item.classList.remove("active");
+				});
+				li.classList.add("active");
+			};
+			list.forEach(item => {
+				item.addEventListener("click", () => {
+					activeLink(item);
+				});
+			});
+		}
+	}, []);
+
+	// 这是第一种渲染方式菜单动画
+	const firstHTML = () => {
+		return (
+			<>
+				<div ref={navigateRef} className="navigation">
+					<ul>
+						<li className="active">
+							<a href="#">
+								<span>
+									<Icon icon="fa-brands:weixin" />
+								</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span>
+									<Icon icon="ant-design:weibo-outlined" />
+								</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span>
+									<Icon icon="basil:qq-solid" />
+								</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span>
+									<Icon icon="mingcute:twitter-line" />
+								</span>
+							</a>
+						</li>
+						<li>
+							<a href="#">
+								<span>
+									<Icon icon="ant-design:aliwangwang-filled" />
+								</span>
+							</a>
+						</li>
+						<div className="indicator">
+							<span></span>
+						</div>
+					</ul>
+				</div>
+			</>
+		);
+	};
+
+	return <>{firstHTML()}</>;
 };
 
-export default floatingMenu;
+export default navigateView;
